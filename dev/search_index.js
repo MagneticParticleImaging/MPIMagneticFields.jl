c77dd94a6f100d46d894037b2b744ca163f4ce38
Documenter.jl <documenter@juliadocs.github.io>
2023-02-06 21:17:52 +0000

    build based on 837bebd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MPIMagneticFields","category":"page"},{"location":"#MPIMagneticFields","page":"Home","title":"MPIMagneticFields","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MPIMagneticFields.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MPIMagneticFields]","category":"page"}]
+}
