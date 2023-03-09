ebe62552281627cef014e8ca4452e1a56da21542
Jonas Schumacher <github735@jonasschumacher.de>
2023-03-09 21:33:39 +0100

    Add `value` methods for ranges

--- src/Common.jl
+++ src/Common.jl
@@ -1,6 +1,6 @@
 #Base.size(S::SquaresVector) = (S.count,)
 Base.IndexStyle(::Type{<:AbstractMagneticField}) = IndexCartesian()
-Base.getindex(field::AbstractMagneticField, r...) = value(field, [r...])
+Base.getindex(field::AbstractMagneticField, r...) = value(field, [r...]) # TODO: How to deal with rotation and translation?
 
 # TODO: This only works with eachindex() available, which is only the case for fields defining a grid. How shall we solve this? Create a container?
 LinearAlgebra.norm(field::AbstractMagneticField) = [norm(field[i]) for i ∈ eachindex(field)]
--- src/MPIMagneticFields.jl
+++ src/MPIMagneticFields.jl
@@ -11,8 +11,12 @@ Abstract supertype for magnetic fields.
 """
 abstract type AbstractMagneticField end
 
-# TODO: Document functions
 export value
+"""
+    $(SIGNATURES)
+
+Retrieve the value of a magnetic field at position `r`.
+"""
 value(::AbstractMagneticField, r::PT) where {T <: Number, PT <: AbstractVector{T}} = error("Not yet implemented")
 value(field::AbstractMagneticField, r::PT, ϕ::RT) where {T <: Number, PT <: AbstractVector{T}, RT <: Number} = isRotatable(field) ? error("Not yet implemented") : value(field, r)
 value(field::AbstractMagneticField, r::PT, δ::TT) where {T <: Number, PT <: AbstractVector{T}, T2 <: Number, TT <: AbstractVector{T2}} = isTranslatable(field) ? error("Not yet implemented") : value(field, r)
@@ -27,6 +31,17 @@ function value(field::AbstractMagneticField, r::PT, ϕ::RT, δ::TT) where {T <: 
     value(field, r)
   end
 end
+
+value(field::AbstractMagneticField, r::PT) where {T <: Union{AbstractRange, Number}, PT <: AbstractVector{T}} = [value(field, [r_...]) for r_ in Iterators.product(r...)]
+value(field::AbstractMagneticField, r::PT, ϕ::RT) where {T <: Union{AbstractRange, Number}, PT <: AbstractVector{T}, RT <: Number} = [value(field, [r_...], ϕ) for r_ in Iterators.product(r...)]
+value(field::AbstractMagneticField, r::PT, δ::TT) where {T <: Union{AbstractRange, Number}, PT <: AbstractVector{T}, T2 <: Number, TT <: AbstractVector{T2}} = [value(field, [r_...], δ) for r_ in Iterators.product(r...)]
+value(field::AbstractMagneticField, r::PT, ϕ::RT, δ::TT) where {T <: Union{AbstractRange, Number}, PT <: AbstractVector{T}, RT <: Number, T2 <: Number, TT <: AbstractVector{T2}} = [value(field, [r_...], ϕ, δ) for r_ in Iterators.product(r...)]
+
+# Mixed vectors of ranges and scalars are Vector{Any} which does not have a dispatch yet; anything else will fail
+value(field::AbstractMagneticField, r::PT) where {T <: Any, PT <: AbstractVector{T}} = value(field, convert(Vector{Union{<:AbstractRange, Number}}, r))
+value(field::AbstractMagneticField, r::PT, ϕ::RT) where {T <: Union{AbstractRange, Number}, PT <: AbstractVector{T}, RT <: Number} = value(field, convert(Vector{Union{<:AbstractRange, Number}}, r), ϕ)
+value(field::AbstractMagneticField, r::PT, δ::TT) where {T <: Union{AbstractRange, Number}, PT <: AbstractVector{T}, T2 <: Number, TT <: AbstractVector{T2}} = value(field, convert(Vector{Union{<:AbstractRange, Number}}, r), δ)
+value(field::AbstractMagneticField, r::PT, ϕ::RT, δ::TT) where {T <: Union{AbstractRange, Number}, PT <: AbstractVector{T}, RT <: Number, T2 <: Number, TT <: AbstractVector{T2}} = value(field, convert(Vector{Union{<:AbstractRange, Number}}, r), ϕ, δ)
 
 include("Traits.jl")
 include("Common.jl")
