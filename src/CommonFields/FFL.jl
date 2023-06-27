f866662ca3c3185b80c3456390c4539ee3ea1808
Jonas Schumacher <github735@jonasschumacher.de>
2023-06-27 11:03:18 +0200

    Make FFL rotate clockwise

--- src/CommonFields/FFL.jl
+++ src/CommonFields/FFL.jl
@@ -10,7 +10,7 @@ GradientFieldStyle(::IdealXYFFL) = FFLGradientField()
 FieldMovementStyle(::IdealXYFFL) = RotationalMovement()
 
 function value_(field::IdealXYFFL, r, ϕ)
-  sincos_ = sincos(ϕ)
+  sincos_ = sincos(-ϕ) # Rotates clockwise
   mat = SMatrix{3, 3}(1/2-1/2*sincos_[2], -1/2*sincos_[1], 0, -1/2*sincos_[1], 1/2+1/2*sincos_[2], 0, 0, 0, 1) .* field.gradient
   return mat * r
 end
